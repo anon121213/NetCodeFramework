ab9017ef73b7ea572a40a758d421c020c47c470d
Arthur <zheldakovartur@gmail.com>
2025-01-31 13:44:05 +0300

    [fix] netRb, netTransform, netObjectsSynker

--- Assets/_Scripts/Netcore/Runner/NetworkRunner.cs
+++ Assets/_Scripts/Netcore/Runner/NetworkRunner.cs
@@ -2,20 +2,27 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
+using System.Reflection;
 using System.Threading;
+using _Scripts.Netcore.Data.Attributes;
 using _Scripts.Netcore.Data.ConnectionData;
 using _Scripts.Netcore.Initializer;
+using _Scripts.Netcore.NetworkComponents.RPCComponents;
 using _Scripts.Netcore.RPCSystem;
+using _Scripts.Netcore.RPCSystem.ProcessorsData;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 namespace _Scripts.Netcore.Runner
 {
-    public class NetworkRunner : INetworkRunner, IDisposable
+    public class NetworkRunner : NetworkService, INetworkRunner, IDisposable
     {
         private readonly CancellationTokenSource _cts = new();
         private readonly IRpcListener _rpcListener;
+        private readonly MethodInfo _methodInfo = typeof(NetworkRunner).GetMethod(nameof(SetPlayerId));
 
+        private bool _isSettedPlayerId;
+        
         public Dictionary<int, Socket> ConnectedClients { get; } = new();
 
         public List<Socket> TcpClientSockets { get; } = new();
@@ -29,20 +36,29 @@ namespace _Scripts.Netcore.Runner
         public int MaxClients { get; private set; }
 
         public bool IsServer { get; private set; }
+        
+        public IPAddress ServerIp { get; private set; }
+
+        public int PlayerId { get; private set; }
 
         public event Action<int> OnPlayerConnected;
+        public event Action OnServerStarted;
+        public event Action OnClientStarted;
 
         public NetworkRunner(IRpcListener rpcListener,
             INetworkInitializer networkInitializer)
         {
             _rpcListener = rpcListener;
             networkInitializer.Initialize(this);
+            RPCInvoker.RegisterRPCInstance<NetworkRunner>(this);
         }
         
         public async UniTask StartServer(ConnectServerData connectServerData)
         {
             SetServerParameters(connectServerData);
-
+            
+            ServerIp = IPAddress.Parse("127.0.0.1");
+            
             TcpServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             TcpServerSocket.Bind(new IPEndPoint(IPAddress.Any, TcpPort));
             TcpServerSocket.Listen(MaxClients);
@@ -56,6 +72,8 @@ namespace _Scripts.Netcore.Runner
 
             Console.WriteLine("Сервер ожидает подключения...");
 
+            OnServerStarted?.Invoke();
+            
             WaitConnectClients(remoteEndPoint).Forget();
         }
 
@@ -76,6 +94,8 @@ namespace _Scripts.Netcore.Runner
             
             UniTask.RunOnThreadPool(() => _rpcListener.ListenForUdpRpcCalls(UdpServerSocket, remoteEndPoint, _cts.Token))
                 .AttachExternalCancellation(_cts.Token);
+            
+            OnClientStarted?.Invoke();
         }
 
         private async UniTaskVoid WaitConnectClients(IPEndPoint remoteEndPoint)
@@ -83,24 +103,36 @@ namespace _Scripts.Netcore.Runner
             while (TcpClientSockets.Count < MaxClients
                    && UdpClientSockets.Count < MaxClients)
             {
-                var clientSocketTCP = await TcpServerSocket.AcceptAsync();
+                var clientSocket = await TcpServerSocket.AcceptAsync();
 
-                int playerIndex = TcpClientSockets.IndexOf(clientSocketTCP);
+                int playerIndex = TcpClientSockets.Count + 1;
 
-                TcpClientSockets.Add(clientSocketTCP);
-                UdpClientSockets.Add(clientSocketTCP);
+                TcpClientSockets.Add(clientSocket);
+                UdpClientSockets.Add(clientSocket);
 
-                ConnectedClients.Add(playerIndex, clientSocketTCP);
+                ConnectedClients.Add(playerIndex, clientSocket);
 
-                UniTask.RunOnThreadPool(() => _rpcListener.ListenForTcpRpcCalls(clientSocketTCP, _cts.Token))
+                UniTask.RunOnThreadPool(() => _rpcListener.ListenForTcpRpcCalls(clientSocket, _cts.Token))
                     .AttachExternalCancellation(_cts.Token);
                 
                 UniTask.RunOnThreadPool(() => _rpcListener.ListenForUdpRpcCalls(UdpServerSocket, remoteEndPoint, _cts.Token))
                     .AttachExternalCancellation(_cts.Token);
 
                 OnPlayerConnected?.Invoke(playerIndex);
-                Debug.Log($"Клиент подключен: {clientSocketTCP.RemoteEndPoint}");
+                await UniTask.Delay(1000);
+                RPCInvoker.InvokeServiceRPC<NetworkRunner>(this, _methodInfo, NetProtocolType.Tcp, playerIndex);
+                Debug.Log($"Клиент подключен: {clientSocket.RemoteEndPoint}");
             }
+        }
+
+        [ClientRPC]
+        public void SetPlayerId(int id)
+        {
+            if (_isSettedPlayerId)
+                return;
+            
+            _isSettedPlayerId = true;
+            PlayerId = id;
         }
 
         private void SetServerParameters(ConnectServerData data)
@@ -112,6 +144,7 @@ namespace _Scripts.Netcore.Runner
 
         public void Dispose()
         {
+            _cts.Cancel();
             _cts.Dispose();
         }
     }
--- Assets/_Scripts/Netcore/Spawner/ObjectsSyncer/NetworkObjectsSyncer.cs
+++ Assets/_Scripts/Netcore/Spawner/ObjectsSyncer/NetworkObjectsSyncer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using _Scripts.Netcore.RPCSystem;
 using _Scripts.Netcore.RPCSystem.ProcessorsData;
@@ -8,35 +9,36 @@ namespace _Scripts.Netcore.Spawner.ObjectsSyncer
 {
     public class NetworkObjectsSyncer : INetworkObjectSyncer
     {
-        private readonly List<(int, GameObject)> _networkObjects = new();
-        private  MethodInfo _spawnMethodInfo;
+        private readonly List<(int, int, GameObject)> _networkObjects = new();
+        private MethodInfo _spawnMethodInfo;
 
-        public void AddNetworkObject(int objectId, GameObject gameObject)
-        {
-            _networkObjects.Add((objectId, gameObject));
-        }
+        public void AddNetworkObject(int objectId, int uniqueId, GameObject gameObject) => 
+            _networkObjects.Add((objectId, uniqueId, gameObject));
 
         public void RemoveNetworkObject(int objectId, GameObject gameObject)
         {
             
         }
 
+        public bool CheckSyncObject(int prefabId, int uniqueId) =>
+            _networkObjects.Any(networkObject => 
+                networkObject.Item1 == prefabId && networkObject.Item2 == uniqueId);
+
         public void Sync(NetworkSpawner networkSpawner)
         {
             _spawnMethodInfo = typeof(NetworkSpawner).GetMethod("SpawnClientRpc");
             
             foreach (var networkObject in _networkObjects)
-            {
                 RPCInvoker.InvokeServiceRPC<NetworkSpawner>(networkSpawner, _spawnMethodInfo,
-                    NetProtocolType.Tcp, networkObject.Item1, networkObject.Item2.transform.position,
-                    networkObject.Item2.transform. rotation, networkObject.Item2.transform.localScale);
-            }
+                    NetProtocolType.Tcp, networkObject.Item1, networkObject.Item2, networkObject.Item3.transform.position,
+                    networkObject.Item3.transform. rotation, networkObject.Item3.transform.localScale);
         }
     }
 
     public interface INetworkObjectSyncer
     {
         void Sync(NetworkSpawner networkSpawner);
-        void AddNetworkObject(int objectId, GameObject gameObject);
+        void AddNetworkObject(int objectId, int uniqueId, GameObject gameObject);
+        bool CheckSyncObject(int prefabId, int uniqueId);
     }
 }
